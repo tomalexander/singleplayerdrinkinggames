22bf12f504d7afc6d0591b7e70db41161a944015
Colin Rice <colin@daedrum.net>
2013-02-16 16:33:56 -0500

    Add nav_bar everywhere, add css everywhere

--- frontend/main.dart
+++ frontend/main.dart
@@ -1,32 +1,11 @@
 import 'dart:html';
-
-class nav_bar
-{
-    DivElement content;
-    nav_bar()
-    {
-        content = new DivElement();
-        content.id = "nav_bar";
-        var link_list = ["Home", "Games", "Booze"];
-        var link_map = {
-            "Home" : "index",
-            "Games" : "games",
-            "Booze" : "booze"
-        };
-        link_list.forEach( (e) {
-            var page = link_map[e];
-            var link_html = new Element.html("<a href=/${page}.html> ${e} </a>");
-            content.children.add(link_html);
-        });
-    }
-}
+import 'nav_bar.dart';
 
 main() {
 
     try {
         main_wrapped();
-    } on Exception
-    catch (ex) {
+    } catch (ex) {
         document.window.alert(ex.toString());
     }
 }
