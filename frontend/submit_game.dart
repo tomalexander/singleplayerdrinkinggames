3184afe6ff6b8a7890879ddaa456c595c8108af0
Ahumm <larset2@gmail.com>
2013-02-19 17:59:35 -0500

    CSS works

--- frontend/submit_game.dart
+++ frontend/submit_game.dart
@@ -11,36 +11,36 @@ class submit_game_form {
     form.method = "POST";
     content.nodes.add(form);
     
-    DivElement de = new Element.html("<div class="submit.row"></div>");
-    de.nodes.add(new Element.html("<label class=\"submit.col1\" for=\"game-name\">Game Name:</label>"));
-    de.nodes.add(new Element.html("<input class=\"submit.col2\" type=\"text\" name=\"game_name\" maxlength=\"255\" placeholder=\"Game Name Goes Here: 255 characters\" required>"));
+    DivElement de = new Element.html("<div class=\"row\"></div>");
+    de.nodes.add(new Element.html("<label class=\"col1\" for=\"game-name\">Game Name:</label>"));
+    de.nodes.add(new Element.html("<input class=\"col2\" type=\"text\" name=\"game-name\" maxlength=\"255\" placeholder=\"Game Name Goes Here: 255 characters\" required>"));
     
     form.nodes.add(de);
     
-    de = new Element.html("<div class="submit.row"></div>");
-    de.nodes.add(new Element.html("<label class=\"submit.col1\" for=\"short-description\">Short Description:</label>"));
-    de.nodes.add(new Element.html("<input class=\"submit.col2\" type=\"text\" name=\"short_description\" maxlength=\"255\" placeholder=\"Short Description: 255 characters\" required>"));
-    
-    form.nodes.add(de)
-    
-    de = new Element.html("<div class="submit.row"></div>");
-    de.nodes.add(new Element.html("<label class=\"submit.col1\" for=\"long-description\">Long Description:</label>"));
-    de.nodes.add(new Element.html("<textarea class=\"submit.col2\" rows=\"4\" name=\"long_description\" maxlength=\"1023\" placeholder=\"Long Description: 1023 characters\" required>"));
+    de = new Element.html("<div class=\"row\"></div>");
+    de.nodes.add(new Element.html("<label class=\"col1\" for=\"short-description\">Short Description:</label>"));
+    de.nodes.add(new Element.html("<input class=\"col2\" type=\"text\" name=\"short-description\" maxlength=\"255\" placeholder=\"Short Description: 255 characters\" required>"));
     
     form.nodes.add(de);
     
-    de = new Element.html("<div class="submit.row"></div>");
-    de.nodes.add(new Element.html("<label class=\"submit.col1\" for=\"supplies\">Supplies:</label>"));
-    DivElement supply_div = new Element.html("<div id=\"supply-div\" class=\"submit.col2\"></div>");
+    de = new Element.html("<div class=\"row\"></div>");
+    de.nodes.add(new Element.html("<label class=\"col1\" for=\"long-description\">Long Description:</label>"));
+    de.nodes.add(new Element.html("<textarea class=\"col2\" rows=\"4\" name=\"long-description\" maxlength=\"1023\" placeholder=\"Long Description: 1023 characters\" required>"));
+    
+    form.nodes.add(de);
+    
+    de = new Element.html("<div class=\"row\"></div>");
+    de.nodes.add(new Element.html("<label class=\"col1\" for=\"supplies\">Supplies:</label>"));
+    DivElement supply_div = new Element.html("<div id=\"supply-div\" class=\"col2\"></div>");
     if(supply_div.nodes.length < 2) {
       add_supply_item(supply_div, first_item : true);
     }
     de.nodes.add(supply_div);
     form.nodes.add(de);
     
-    de = new Element.html("<div class="submit.row"></div>");
-    de.nodes.add(new Element.html("<label class=\"submit.col1\" for=\"instructions\">Instructions:</label>"));
-    DivElement instruction_div = new Element.html("<div id=\"instruction-div\" class=\"submit.col2\"></div>");
+    de = new Element.html("<div class=\"row\"></div>");
+    de.nodes.add(new Element.html("<label class=\"col1\" for=\"instructions\">Instructions:</label>"));
+    DivElement instruction_div = new Element.html("<div id=\"instruction-div\" class=\"col2\"></div>");
     if(instruction_div.nodes.length < 2) {
       add_instruction_item(instruction_div, first_item : true);
     }
@@ -80,17 +80,17 @@ class submit_game_form {
   }
   
   void add_supply_item(DivElement to_insert_after, {bool first_item : false}) {
-    DivElement new_item = new Element.html("<div class=\"submit.item\"></div>");
-    Element new_box = new Element.html("<input id=\"supplies\" name=\"supplies[]\" class=\"submit.supplies\" type=\"text\" placeholder=\"Add Supply Here: 255 characters\" maxlength=\"255\">");
-    Element new_add_button = new Element.html("<input id=\"add-supply-button\" name=\"add-supply-button\" class=\"submit.add-supply-button\" type=\"button\" value=\"+\">");
-    Element new_remove_button = new Element.html("<input id=\"remove-supply-button\" name=\"remove-supply-button\" class=\"submit.remove-supply-button\" type=\"button\" value=\"-\">");
+    DivElement new_item = new Element.html("<div class=\"item\"></div>");
+    Element new_box = new Element.html("<input id=\"supplies\" name=\"supplies[]\" class=\"supplies\" type=\"text\" placeholder=\"Add Supply Here: 255 characters\" maxlength=\"255\">");
+    Element new_add_button = new Element.html("<input id=\"add-supply-button\" name=\"add-supply-button\" class=\"add-supply-button\" type=\"button\" value=\"+\">");
+    Element new_remove_button = new Element.html("<input id=\"remove-supply-button\" name=\"remove-supply-button\" class=\"remove-supply-button\" type=\"button\" value=\"-\">");
     
     new_add_button.onClick.listen((e) => add_supply_item(new_item));
     new_remove_button.onClick.listen((e) { 
                                            // grab [parent]'s [parent], remove [parent], add a new item if none exist
                                            DivElement parent_parent = new_add_button.parent.parent;
                                            new_add_button.parentNode.remove();
-                                           if(parent_parent.nodes.length < 2){
+                                           if(parent_parent.nodes.length < 1){
                                              add_supply_item(parent_parent, first_item : true);
                                            }
                                          });
@@ -107,17 +107,17 @@ class submit_game_form {
   }
   
   void add_instruction_item(DivElement to_insert_after, {bool first_item : false}) {
-  DivElement new_item = new Element.html("<div class=\"submit.item\"></div>");
-  Element new_box = new Element.html("<input id=\"instructions\" name=\"instructions[]\" class=\"submit.instructions\" type=\"text\" placeholder=\"Add Instruction Here: 255 characters\" maxlength=\"255\">");
-  Element new_add_button = new Element.html("<input id=\"add-instruction-button\" name=\"add-instruction-button\" class=\"submit.add-instruction-button\" type=\"button\" value=\"+\">");
-  Element new_remove_button = new Element.html("<input id=\"remove-instruction-button\" name=\"remove-instruction-button\" class=\"submit.add-instruction-button\" type=\"button\" value=\"-\">");
+  DivElement new_item = new Element.html("<div class=\"item\"></div>");
+  Element new_box = new Element.html("<input id=\"instructions\" name=\"instructions[]\" class=\"instructions\" type=\"text\" placeholder=\"Add Instruction Here: 255 characters\" maxlength=\"255\">");
+  Element new_add_button = new Element.html("<input id=\"add-instruction-button\" name=\"add-instruction-button\" class=\"add-instruction-button\" type=\"button\" value=\"+\">");
+  Element new_remove_button = new Element.html("<input id=\"remove-instruction-button\" name=\"remove-instruction-button\" class=\"add-instruction-button\" type=\"button\" value=\"-\">");
 
   new_add_button.onClick.listen((e) => add_instruction_item(new_item));
   new_remove_button.onClick.listen((e) { 
                                          // grab [parent]'s [parent], remove [parent], add a new item if none exist
                                          DivElement parent_parent = new_add_button.parent.parent;
                                          new_add_button.parentNode.remove();
-                                         if(parent_parent.nodes.length < 2){
+                                         if(parent_parent.nodes.length < 1){
                                            add_instruction_item(parent_parent, first_item : true);
                                          }
                                        });
