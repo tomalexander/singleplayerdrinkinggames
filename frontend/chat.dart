d1d8bec12d6a52023172c93449b8cbc0b80fea27
Tom Alexander <craftkiller@gmail.com>
2013-03-26 17:24:48 -0400

    syncing to go to class

--- frontend/chat.dart
+++ frontend/chat.dart
@@ -7,14 +7,24 @@ class chat_box
     String room;
     DivElement content;
     TextAreaElement chat_window;
+    InputElement input_message;
+    ButtonElement submit_message;
 
     chat_box(String _room) {
         room = _room;
         content = new Element.html("<div></div>");
-        FormElement form = new Element.html("<form action=\"chat.php\" method=\"POST\"></form>");
-        content.nodes.add(form);
         chat_window = new Element.html("<textarea></textarea>");
+        input_message = new Element.html("<input name=\"message\" required>");
+        submit_message = new ButtonElement();
+        submit_message..text = "send"
+            ..onClick.listen((e) => send_message());
         content.nodes.add(chat_window);
+        content.nodes.add(input_message);
+        content.nodes.add(submit_message);
+    }
+
+    void send_message() {
+        document.window.alert("send_message");
     }
 }
 
@@ -28,5 +38,5 @@ void main() {
 }
 
 void main_wrapped() {
-    query('#main').children.add(new chat_box().content);
+    query('#main').children.add(new chat_box("main").content);
 }
