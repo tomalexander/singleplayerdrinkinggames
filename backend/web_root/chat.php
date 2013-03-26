262a65fab52eee26f85dab538821e685a7ca8c13
Tom Alexander <craftkiller@gmail.com>
2013-03-26 16:49:02 -0400

    initial code to send messages

--- backend/web_root/chat.php
+++ backend/web_root/chat.php
@@ -12,18 +12,40 @@ class chat_message
     public $timestamp;
 }
 
-if ($action == "send")
+function cleanup_old_chats()
 {
+    $db = open_db();
+    $db->exec("DELETE FROM `singleplayerdb`.`chat` WHERE `chat`.`time` < DATE_SUB(NOW(),INTERVAL '60' SECOND)");
+    close_db();
+}
+
+if ($action == "send") {
     // Post a message
     $uuid = $_REQUEST["uuid"];
+    $room = isset($_REQUEST["room"]) ? $_REQUEST["room"] : "main";
+    $message = $_REQUEST["message"];
+    $db = open_db();
+    cleanup_old_chats();
     $user_info = get_login($uuid);
     if ($user_info == null) {
         die("{\n    \"error\": \"UUID NOT LOGGED IN\"\n}");
     }
+    $insert = "INSERT INTO chat (userid, room, message) VALUES (:userid, :room, :message);";
+    $stmt = $db->prepare($insert);
+    $stmt->bindParam(":userid", $user_info->id);
+    $stmt->bindParam(":room", $room);
+    $stmt->bindParam(":message", $message);
+    if (! $stmt->execute()) {
+        close_db();
+        die("{\n    \"error\": \"Failed to execute statement\"\n}");
+    }
+    close_db();
 } else if ($action == "get") {
-    $room = $_REQUEST["room"];
+    //Retrieve messages
+    $room = isset($_REQUEST["room"]) ? $_REQUEST["room"] : "main";
+    $last_id = isset($_REQUEST["last_id"]) ? $_REQUEST["last_id"] : 0;
     $db = open_db();
-    $result = $db->query("SELECT chat.id,chat.message,UNIX_TIMESTAMP(chat.time),(SELECT username FROM users WHERE id=chat.userid LIMIT 1) FROM chat WHERE `room`=\"{$room}\";");
+    $result = $db->query("SELECT chat.id,chat.message,UNIX_TIMESTAMP(chat.time),(SELECT username FROM users WHERE id=chat.userid LIMIT 1) FROM chat WHERE `room`=\"{$room}\" AND `id`>{$last_id};");
     $ret = array();
     foreach ($result as $row) {
         $current = new chat_message;
