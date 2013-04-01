f9221c42b9466b1308312225f4b55730936b2614
Tom Alexander <craftkiller@gmail.com>
2013-04-01 14:15:09 -0400

    merged the form validation css into the less file

--- deploy.sh
+++ deploy.sh
@@ -38,7 +38,7 @@ REMOTEUSER="theenablers"
 HOST="singleplayerdrinkinggames.com"
 
 rsync -rvz --copy-links backend/web_root/* $REMOTEUSER@$HOST:~/singleplayerdrinkinggames.com
-rsync -rvz --copy-links frontend/*.js frontend/*.html frontend/*.less frontend/*.css $REMOTEUSER@$HOST:~/singleplayerdrinkinggames.com
+rsync -rvz --copy-links frontend/*.js frontend/*.html frontend/*.less $REMOTEUSER@$HOST:~/singleplayerdrinkinggames.com
 rsync -avz backend/db_functions.php $REMOTEUSER@$HOST:~/
 
 HASH=`git rev-parse HEAD`
