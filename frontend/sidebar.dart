08cf4bbfa7d95cf26a9cc26e8841ac5301dbdcce
Colin Rice <colin@daedrum.net>
2013-04-08 16:44:18 -0400

    Fix sidebar css and show links

--- frontend/sidebar.dart
+++ frontend/sidebar.dart
@@ -1,6 +1,8 @@
 library sidebar;
 import 'dart:html';
+import 'dart:json';
 import 'login.dart';
+import 'util.dart';
 
 class sidebar {
   DivElement content;
@@ -8,7 +10,18 @@ class sidebar {
   sidebar() {
     content = new DivElement();
     content.classes.add('sidebar');
-    content.text = "SIDEBAR";
+    get_string("sidebar_games.php", "", (resp) {
+        List games = parse(resp);
+        games.forEach( (Map game) {
+            DivElement InfoDiv = new DivElement();
+            InfoDiv.classes.add('game_name');
+            var gamename = game["game_name"];
+            var gameid = game["game_id"];
+            var link = new Element.html("<a href=/#page=view_ga    me;gameid=${gameid}>${gamename}</a>");
+            InfoDiv.children.add(link);
+            content.children.add(InfoDiv);
+        });
+    });
   }
 }
 
