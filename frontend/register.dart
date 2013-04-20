a822dfee2c00675a72c2513838ce28565f1c87f2
Tom Alexander <craftkiller@gmail.com>
2013-04-20 18:46:50 -0400

    register form formatted with css

--- frontend/register.dart
+++ frontend/register.dart
@@ -9,19 +9,26 @@ class register_form
       /*
        * Creates the form that allows a user to register on the website.
        */
-        content = new Element.html("<div></div>");
+        content = new Element.html("<div id=\"login-form\"></div>");
         FormElement form = new Element.html("<form action=\"register.php\" method=\"POST\"></form>");
         content.nodes.add(form);
-        form.nodes.add(new Text("Username:"));
-        form.nodes.add(new Element.html("<input type=\"text\" name=\"username\" required>"));
-        form.nodes.add(new Element.html("<br>"));
-        form.nodes.add(new Text("Password:"));
-        InputElement password = new Element.html("<input type=\"password\" name=\"password\" required>");
-        form.nodes.add(password);
-        form.nodes.add(new Element.html("<br>"));
-        form.nodes.add(new Text("Confirm Password:"));
-        InputElement confirm_password = new Element.html("<input type=\"password\" name=\"confirm_password\" required>");
-        form.nodes.add(confirm_password);
+
+        DivElement de = new Element.html("<div class=\"row\"></div>");
+        de.nodes.add(new Element.html("<span style=\"label\">Username:</span>"));
+        de.nodes.add(new Element.html("<input type=\"text\" name=\"username\" class=\"input\" required>"));
+        form.nodes.add(de);
+
+        de = new Element.html("<div class=\"row\"></div>");
+        de.nodes.add(new Element.html("<span style=\"label\">Password:</span>"));
+        InputElement password = new Element.html("<input type=\"password\" name=\"password\" class=\"input\" required>");
+        de.nodes.add(password);
+        form.nodes.add(de);
+
+        de = new Element.html("<div class=\"row\"></div>");
+        de.nodes.add(new Element.html("<span style=\"label\">Confirm Password:</span>"));
+        InputElement confirm_password = new Element.html("<input type=\"password\" name=\"confirm_password\" class=\"input\" required>");
+        de.nodes.add(confirm_password);
+        form.nodes.add(de);
         confirm_password.onInput.listen((e) {
           //Checks to see if the passwords match
                 if (password.value != confirm_password.value) {
@@ -30,12 +37,16 @@ class register_form
                     confirm_password.setCustomValidity("");
                 }
             });
-        form.nodes.add(new Element.html("<br>"));
-        form.nodes.add(new Text("E-Mail:"));
-        form.nodes.add(new Element.html("<input type=\"email\" name=\"email\" required>"));
-        form.nodes.add(new Element.html("<br>"));
-        SubmitButtonInputElement submit = new Element.html("<input type=\"submit\" value=\"Create Account\">");
-        form.nodes.add(submit);
+        
+        de = new Element.html("<div class=\"row\"></div>");
+        de.nodes.add(new Element.html("<span style=\"label\">E-Mail:</span>"));
+        de.nodes.add(new Element.html("<input type=\"email\" name=\"email\" class=\"input\" required>"));
+        form.nodes.add(de);
+
+        de = new Element.html("<div class=\"row\"></div>");
+        SubmitButtonInputElement submit = new Element.html("<input type=\"submit\" value=\"Create Account\" class=\"submit\">");
+        de.nodes.add(submit);
+        form.nodes.add(de);
         form.onSubmit.listen((e) {submit.disabled = true;});
     }
 }
